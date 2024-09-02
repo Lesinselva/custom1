6bf1e214f11b2c1f5100361ec7f95fe07e02bdb6
Lesinselva <lesinselva@gmail.com>
2024-09-02 09:05:03 +0530

    Your descriptive commit message

--- lib/custom.dart
+++ lib/custom.dart
@@ -1,7 +1,5 @@
 library custom;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'appbar.dart';
+export 'profile.dart';
+export 'tile.dart';

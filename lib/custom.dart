facbc8f830da8b3a258dda038f9d6a8dcc09874c
Lesinselva <lesinselva@gmail.com>
2024-09-02 09:07:30 +0530

    Your descriptive commit message

--- lib/custom.dart
+++ lib/custom.dart
@@ -1,5 +1,47 @@
 library custom;
 
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+import 'appbar.dart';
+import 'profile.dart';
+import 'tile.dart';
+
 export 'appbar.dart';
 export 'profile.dart';
 export 'tile.dart';
+
+class Custom {
+  static CustomAppBar appBar(String title) => CustomAppBar(title: title);
+
+  static ProfileImagePicker picker({
+    required void Function(File? image) onImagePicked,
+    required String username,
+    required String number,
+    required IconData pickericon,
+    required IconData conIcon,
+  }) {
+    return ProfileImagePicker(
+      onImagePicked: onImagePicked,
+      username: username,
+      number: number,
+      pickericon: pickericon,
+      conIcon: conIcon,
+    );
+  }
+
+  static ProfileTile profile({
+    required String title,
+    String? subtitle,
+    required IconData icon,
+    IconData? trailingIcon,
+  }) {
+    return ProfileTile(
+      title: title,
+      subtitle: subtitle,
+      icon: icon,
+      trailingIcon: trailingIcon,
+    );
+  }
+}
